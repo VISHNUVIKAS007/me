1d18ddee2ac3b25cd4eae5f98c9c6892d292bddc
vishnu <vishnu@example.com>
2019-10-30 22:31:03 +0530

    last

---
+++ src/resumeData.js
@@ -0,0 +1,7 @@
+let resumeData={
+    "name":"Vishnu S",
+    "role":"Software Developer",
+    "description":"I'm passionate about Computer Software development, looking always for different and most efficient solutions to daily problems of clients. An enthusiast of new technologies and creations in Software development. Always growing as a person and as a co-worker, looking forward to giving my best for the team."
+}
+
+export default resumeData;
